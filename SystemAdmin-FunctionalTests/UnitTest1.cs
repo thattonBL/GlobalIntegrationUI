4d4e4ffab741da1228349bd6515c51f6f2bbb8fe
thatton <thomasrichardhatton@googlemail.com>
2025-01-10 14:56:59 +0000

    Restructure of Project after Adding Automated test projects and Versioning of Container

---
+++ SystemAdmin-FunctionalTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace SystemAdmin_FunctionalTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PlaceholderTest()
+        {
+            var myString = "Someone needs to write some blooming functional tests for this...";
+            Assert.True(myString.Length > 2);
+        }
+    }
+}
---
+++ SystemAdmin-UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace SystemAdmin_UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PlaceholderTest()
+        {
+            var myString = "Someone needs to write some blooming unit tests for this...";
+            Assert.True(myString.Length > 2);
+        }
+    }
+}
